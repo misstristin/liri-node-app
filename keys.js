07d66c5ddbcb3f1d6f85b3119f98c5840ff77696
Tristin Tobias <tristintobias@gmail.com>
2018-07-31 23:28:00 -0700

    adding initial files with all keys added and packages installed.

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.BlRVsT2A08JaxM9wXyhSE9AHA,
+  consumer_secret: process.env.mH2KLXRzPDJ4XvOYycAr9fpAhDNx0hAAHgAt9DYcS49XHLaTT7, 
+  access_token_key: process.env.33848279-bDS71GztowLN5XLVhDR5F5WL3ml9MSzbOE7LVDByh,
+  access_token_secret: process.env.A0IXKAgv2qXiXkI2sCnKkjVx6hFkBMAwtIZ1AJ7OMjMNi
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_864bc8471f8a403b92dfc7a8cac16de3,
+  secret: process.env.SPOTIFY_b9dce90ea7e24d989ec721e677021eaf
+};
