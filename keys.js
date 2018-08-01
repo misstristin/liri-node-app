003a18841114683aa0efebc610f35fc18040205e
Tristin Tobias <tristintobias@gmail.com>
2018-08-01 13:42:41 -0700

    started from scratch. added all files and packages.

--- keys.js
+++ keys.js
@@ -1,13 +1,13 @@
-console.log('this is loaded');
+console.log('Keys are loaded.');
 
 exports.twitter = {
-  consumer_key: process.env.BlRVsT2A08JaxM9wXyhSE9AHA,
-  consumer_secret: process.env.mH2KLXRzPDJ4XvOYycAr9fpAhDNx0hAAHgAt9DYcS49XHLaTT7, 
-  access_token_key: process.env.33848279-bDS71GztowLN5XLVhDR5F5WL3ml9MSzbOE7LVDByh,
-  access_token_secret: process.env.A0IXKAgv2qXiXkI2sCnKkjVx6hFkBMAwtIZ1AJ7OMjMNi
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_864bc8471f8a403b92dfc7a8cac16de3,
-  secret: process.env.SPOTIFY_b9dce90ea7e24d989ec721e677021eaf
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
